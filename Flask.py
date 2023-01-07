342029b4f38d09463006c93946f6c33f456ea4c5
choiyun9yu <sww2tapple@gmail.com>
2023-01-07 20:22:26 +0900

    main

--- Flask.py
+++ Flask.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
+# 랜더 템플릿은 현재 실행되고 있는 파일과 같은 경로에 있는 templates 폴더 내 HTML문서를 불러오는 모듈
 
-app=Flask()
+app=Flask(__name__)
 
-@app.route('/', method=['GET', "POST"])
+@app.route('/', methods=['GET', "POST"])
 def main():
-    return 'Hello World'
+    return render_template(['main.html'])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
